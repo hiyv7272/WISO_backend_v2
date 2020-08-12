8c8099bd20e7b632a8f6ec2059005e4767373ee0
Inyong Pang <hiyv7272@gmail.com>
2020-08-12 16:21:03 +0900

    DRF Refactring (user) - print 주석 제거 - class명 일부 수정

--- user/serializers.py
+++ user/serializers.py
@@ -11,7 +11,6 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
-        print('validated_data: ', validated_data)
         hashed_password = bcrypt.hashpw(validated_data['password'].encode('utf-8'), bcrypt.gensalt()).decode()
         user = User.objects.create(
             name=validated_data['name'],
@@ -32,15 +31,11 @@ class UserUpdateSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'mobile_number', 'update_datetime']
 
     def update(self, instance, validated_data):
-        # validated_data['update_datetime'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print('update!!')
-        print('instance: ', instance)
-        print('instance.dir :', dir(instance))
-        print('validated_data: ', validated_data)
         instance.name = validated_data.get('name', instance.name)
         instance.mobile_number = validated_data.get('mobile_number', instance.mobile_number)
         instance.update_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         instance.save()
+
         return instance
 
 
@@ -50,12 +45,8 @@ class UserDeleteSerializer(serializers.ModelSerializer):
         fields = ['id', 'update_datetime', 'is_use']
 
     def update(self, instance, validated_data):
-        print('update!!')
-        print('instance: ', instance)
-        print('instance.dir :', dir(instance))
-        print('validated_data: ', validated_data)
         instance.update_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         instance.is_use = False
         instance.save()
-        print('fin instance: ', instance)
+
         return instance
--- user/urls.py
+++ user/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from .views import SignUpView, SignInView, KakaologinView, UserProfileView
+from .views import SignUpView, SignInView, KakaologinView, UserViewSet
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
     path('/signin', SignInView.as_view()),
     path('/kakaologin', KakaologinView.as_view()),
-    path('/list', UserProfileView.as_view({'get': 'list'})),
-    path('/profile', UserProfileView.as_view({
+    path('/list', UserViewSet.as_view({'get': 'list'})),
+    path('/profile', UserViewSet.as_view({
         'post': 'create',
         'get': 'retrieve',
         'put': 'update',
     })),
-    path('/delete', UserProfileView.as_view({'delete': 'delete'})),
+    path('/delete', UserViewSet.as_view({'delete': 'delete'})),
 ]
